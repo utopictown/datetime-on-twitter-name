b195ed0a55b2d44fa50b2a9d66667db19cd9af68
alpha <alpha>
2022-04-02 04:41:22 +0700

    init commit

---
+++ run.py
@@ -0,0 +1,25 @@
+from requests_oauthlib import OAuth1Session
+import os
+from py_dotenv import read_dotenv
+import schedule
+import time
+from datetime import datetime
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+read_dotenv(dotenv_path)
+
+twitter = OAuth1Session(
+    os.getenv('CLIENT_KEY'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    resource_owner_key=os.getenv('ACCESS_TOKEN'),
+    resource_owner_secret=os.getenv('ACCESS_TOKEN_SECRET'))
+
+def update_twitter_name():
+    name = datetime.now().strftime('now%%3A %d %B %Y %H%%3A%M')
+    res = twitter.post('https://api.twitter.com/1.1/account/update_profile.json?name=' + name)
+  
+schedule.every(1).minutes.do(update_twitter_name)
+  
+while True:
+    schedule.run_pending()
+    time.sleep(1)
