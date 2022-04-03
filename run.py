17cb8f4cf94c4bdec4278becbb1d9c6e53c5ebba
utopictown <utopictown@merrve.com>
2022-04-03 14:08:28 +0700

    feat: :sparkles: add utc offset

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from requests_oauthlib import OAuth1Session
 import os
 from py_dotenv import read_dotenv
-import schedule
-import time
 from datetime import datetime
 from pytz import timezone
+from urllib import parse
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 read_dotenv(dotenv_path)
@@ -17,7 +16,9 @@ twitter = OAuth1Session(
 
 def update_twitter_name():
     tz = timezone('Asia/Jakarta')
-    name = datetime.now(tz).strftime('now%%3A %d %B %Y %H%%3A%M')
+    offset = round(tz.utcoffset(datetime.now()).total_seconds() / 3600)
+    prepend = "+" if offset >= 0 else ""
+    name = parse.quote(f"{datetime.now(tz).strftime('now: %d %B %Y %H:%M')} UTC {prepend}{offset}")
     twitter.post('https://api.twitter.com/1.1/account/update_profile.json?name=' + name)
 
 update_twitter_name()
