81b6af570bc02dcb8ad42ad4198388b1633a89f3
utopictown <utopictown@merrve.com>
2022-04-03 14:50:27 +0700

    feat: :sparkles: enable dynamic timezone

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ from requests_oauthlib import OAuth1Session
 import os
 from py_dotenv import read_dotenv
 from datetime import datetime
-from pytz import timezone
+from pytz import timezone, all_timezones
 from urllib import parse
+import sys
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 read_dotenv(dotenv_path)
@@ -14,17 +15,27 @@ twitter = OAuth1Session(
     resource_owner_key=os.getenv('ACCESS_TOKEN'),
     resource_owner_secret=os.getenv('ACCESS_TOKEN_SECRET'))
 
-def update_twitter_name():
-    tz = timezone('Asia/Jakarta')
+def update_twitter_name(tz):
     offset = round(tz.utcoffset(datetime.now()).total_seconds() / 3600)
     prepend = "+" if offset >= 0 else ""
     name = parse.quote(f"{datetime.now(tz).strftime('now: %d %B %Y %H:%M')} UTC {prepend}{offset}")
     twitter.post('https://api.twitter.com/1.1/account/update_profile.json?name=' + name)
 
-update_twitter_name()
-  
-# schedule.every(5).seconds.do(update_twitter_name)
-  
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+offset_input = ""
+
+try:
+    sys.argv[1]
+except:
+    raise Exception('Please provide timezone')
+
+offset_input = sys.argv[1].replace(" ", "")
+
+if len(offset_input):
+    if offset_input not in all_timezones:
+        raise Exception('Invalid Timezone')
+else:
+    raise Exception('Please provide timezone')
+    
+tz = timezone(offset_input)
+
+update_twitter_name(tz)
